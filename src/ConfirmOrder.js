3c4a30ecfaa1aa091949da92f2a23508b677a8f7
Morin Sultan <sultanmorin2001@gmail.com>
2025-12-23 08:36:01 +0100

    feat: enhance ConfirmOrder and Overlay components with order confirmation details and new Icon component

--- src/ConfirmOrder.js
+++ src/ConfirmOrder.js
@@ -1,18 +1,24 @@
-export default function ConfirmOrder({ totalOrder }) {
+export default function ConfirmOrder({
+  handleCheckOut,
+  className = "",
+  totalOrderPrice,
+}) {
   return (
     <div className="confirm-order">
       <div className="total">
         <span>Order Total</span>
-        <span className="total-price">${totalOrder.toFixed(2, 0)}</span>
+        <span className="total-price">${totalOrderPrice.toFixed(2)}</span>
       </div>
-      <div className="type-of-delivery">
+      <div className={`${className} type-of-delivery`}>
         <img
           src="/assets/images/icon-carbon-neutral.svg"
           alt="carbon-netural img"
         ></img>
         This is a <span> carbon-netural </span> delivery
       </div>
-      <button>Confirm Order</button>
+      <button className="action-btn" onClick={() => handleCheckOut()}>
+        Confirm Order
+      </button>
     </div>
   );
 }
--- src/Overlay.js
+++ src/Overlay.js
@@ -1,5 +1,63 @@
-function Overlay() {
-  return <div></div>;
+function Overlay({cartItems, handleStartNewOrder}) {
+  const totalOrderPrice = cartItems.reduce(
+    (acc, cur) => acc + cur.totalPrice,
+    0
+  );
+
+  return (
+    <div className="confirmed-container">
+      <div className="confirmed-details">
+        <div>
+          <img
+            src="/assets/images/icon-order-confirmed.svg"
+            alt="order-confirmed"
+          />
+        </div>
+
+        <h2>Order Confirmed</h2>
+        <p>We hope you enjoy your food!</p>
+        <div className="orders">
+          {cartItems.map((item) => (
+            <Order cartItems={item} key={item.id} />
+          ))}
+
+          <div className="total">
+            <span>Order Total</span>
+            <span className="total-price">${totalOrderPrice.toFixed(2)}</span>
+          </div>
+        </div>
+        <button
+          aria-label="confirm order"
+          onClick={() => handleStartNewOrder()}
+          className="action-btn"
+        >
+          Start New Order
+        </button>
+      </div>
+    </div>
+  );
+}
+function Order({cartItems}) {
+  return (
+    <div className="order">
+      <div className="order-details">
+        <img src={cartItems.image.thumbnail} alt="order-image" />
+        <div>
+          <p className="item-name">{cartItems.name}</p>
+          <div className="item-price-details">
+            <span className="item-quantity">{cartItems.quantity}x</span>
+            <span className="item-price">
+              <span>@</span>
+              {` $${cartItems.price.toFixed(2)}`}
+            </span>
+          </div>
+        </div>
+      </div>
+      <span className="item-totalprice">
+        ${cartItems.totalPrice.toFixed(2)}
+      </span>
+    </div>
+  );
 }
 
 export default Overlay;
